1832fd818f8979ce033804f9fd379a5bde89a217
PSkrabneu <skrebnev@gmail.com>
2024-06-03 14:23:27 +0200

    UtilGames.java created with util methods: getWinnersList, getWinnsMap

--- src/test/java/org/manipulations/test/files/TestFiles.java
+++ src/test/java/org/manipulations/test/files/TestFiles.java
@@ -1,10 +1,13 @@
 package org.manipulations.test.files;
 
 import java.io.IOException;
+import java.util.List;
 import org.manipulations.file.reading.ReadCsv;
 import org.manipulations.file.reading.ReadJson;
 import org.manipulations.file.reading.ReadTxtFile;
 import org.junit.jupiter.api.Test;
+import org.objects.game.Game;
+import org.objects.game.UtilGames;
 
 public class TestFiles {
 
@@ -27,6 +30,19 @@ public class TestFiles {
     ReadCsv.getPeople().stream()
         .skip(10)
         .limit(5)
+        .forEach(System.out::println);
+  }
+
+  @Test
+  public void testWinnersList() {
+    UtilGames.getWinnersList().stream()
+        .limit(10)
+        .forEach(System.out::println);
+  }
+
+  @Test
+  public void testWinnersMap() {
+    UtilGames.getWinnsMap(UtilGames.getWinnersList()).entrySet()
         .forEach(System.out::println);
   }
 }
