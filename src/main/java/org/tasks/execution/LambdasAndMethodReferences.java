aad177a3b1d5e6b8d080d62a20fd212933288470
PSkrabneu <skrebnev@gmail.com>
2024-06-07 10:14:16 +0200

    Task2: Lambda and MethodReference completed

--- src/main/java/org/tasks/execution/LambdasAndMethodReferences.java
+++ src/main/java/org/tasks/execution/LambdasAndMethodReferences.java
@@ -4,33 +4,45 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class LambdasAndMethodReferences {
 
   public static void main(String[] args) {
-    staticMR();
+//    staticMR();
+    boundMR();
 
   }
 
   public static void staticMR() {
     List<Integer> listInt = Arrays.asList(1, 2, 7, 4, 5);
-    Consumer<List<Integer>> consIntLambda = list -> Collections.sort(list);
+    Consumer<List<Integer>> sortListLambda = list -> Collections.sort(list);
 
     System.out.println("Before sorting: " + listInt);
 
-    consIntLambda.accept(listInt);
+    sortListLambda.accept(listInt);
     System.out.println("After sorting: " + listInt);
 
     Collections.shuffle(listInt);
     System.out.println("Before sorting: " + listInt);
 
-    Consumer<List<Integer>> consIntMR = Collections::sort;
-    consIntMR.accept(listInt);
+    Consumer<List<Integer>> sortListMR = Collections::sort;
+    sortListMR.accept(listInt);
     System.out.println("After sorting: " + listInt);
   }
 
   public static void boundMR() {
 
+    String name = "Mr. Joe Bloggs";
+    Predicate<String> isStartedWithLambda = pre -> name.startsWith(pre);
+
+    System.out.println("Started with ? 'Mr.' = " + isStartedWithLambda.test("Mr."));
+    System.out.println("Started with ? 'Ms.' = " + isStartedWithLambda.test("Ms."));
+
+    Predicate<String> isStartedWithMR = name::startsWith;
+
+    System.out.println("Started with ? 'Mr.' = " + isStartedWithMR.test("Mr."));
+    System.out.println("Started with ? 'Ms.' = " + isStartedWithMR.test("Ms."));
   }
 
   public static void unboundMR() {
