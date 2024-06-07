15a59e7a2822d35994ec3d88f4312b678a3d942e
PSkrabneu <skrebnev@gmail.com>
2024-06-07 17:19:14 +0200

    Method 'supplier()' implemented

--- src/main/java/org/tasks/execution/LambdasAndMethodReferences.java
+++ src/main/java/org/tasks/execution/LambdasAndMethodReferences.java
@@ -14,100 +14,119 @@ import java.util.function.Supplier;
 // link: https://luxoft.udemy.com/course/ocp11_from_oca8/
 public class LambdasAndMethodReferences {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+//    consumer();
+        supplier();
 //    staticMR();
 //    boundMR();
 //    unboundMR();
-    constructorMR();
-  }
+//    constructorMR();
+    }
 
-  public static void staticMR() {
-    List<Integer> listInt = Arrays.asList(1, 2, 7, 4, 5);
-    Consumer<List<Integer>> sortListLambda = list -> Collections.sort(list);
+    public static void supplier() {
+        Retrievable getBack = () -> 77;
+        System.out.println("With 'Retr': " + getBack.retrieve());
 
-    System.out.println("Before sorting: " + listInt);
+        Supplier<Integer> print77 = () -> {
+            System.out.println(77);
+            return 0;
+        };
 
-    sortListLambda.accept(listInt);
-    System.out.println("After sorting: " + listInt);
+        System.out.println("With 'Supplier': " + print77.get());
+    }
 
-    Collections.shuffle(listInt);
-    System.out.println("Before sorting: " + listInt);
+    public static void consumer() {
+        Printable<String> printableLambda = s -> System.out.println(s);
+        printableLambda.print("Printable lambda");
+    }
 
-    Consumer<List<Integer>> sortListMR = Collections::sort;
-    sortListMR.accept(listInt);
-    System.out.println("After sorting: " + listInt);
-  }
+    public static void staticMR() {
+        List<Integer> listInt = Arrays.asList(1, 2, 7, 4, 5);
+        Consumer<List<Integer>> sortListLambda = list -> Collections.sort(list);
 
-  public static void boundMR() {
+        System.out.println("Before sorting: " + listInt);
 
-    String name = "Mr. Joe Bloggs";
+        sortListLambda.accept(listInt);
+        System.out.println("After sorting: " + listInt);
 
-    Predicate<String> isStartedWithLambda = pre -> name.startsWith(pre);
+        Collections.shuffle(listInt);
+        System.out.println("Before sorting: " + listInt);
 
-    System.out.println("Started with ? 'Mr.' = " + isStartedWithLambda.test("Mr."));
-    System.out.println("Started with ? 'Ms.' = " + isStartedWithLambda.test("Ms."));
+        Consumer<List<Integer>> sortListMR = Collections::sort;
+        sortListMR.accept(listInt);
+        System.out.println("After sorting: " + listInt);
+    }
 
-    Predicate<String> isStartedWithMR = name::startsWith;
+    public static void boundMR() {
 
-    System.out.println("Started with ? 'Mr.' = " + isStartedWithMR.test("Mr."));
-    System.out.println("Started with ? 'Ms.' = " + isStartedWithMR.test("Ms."));
-  }
+        String name = "Mr. Joe Bloggs";
 
-  public static void unboundMR() {
-    String pre1 = "Mr.";
-    String pre2 = "Ms.";
-    String name = "Mr. Joe Bloggs";
+        Predicate<String> isStartedWithLambda = pre -> name.startsWith(pre);
 
-    Predicate<String> isEmptyLambda = s -> s.isEmpty();
+        System.out.println("Started with ? 'Mr.' = " + isStartedWithLambda.test("Mr."));
+        System.out.println("Started with ? 'Ms.' = " + isStartedWithLambda.test("Ms."));
 
-    System.out.println("Text empty? = " + isEmptyLambda.test(""));
-    System.out.println("Text empty? = " + isEmptyLambda.test("xyz"));
+        Predicate<String> isStartedWithMR = name::startsWith;
 
-    Predicate<String> isEmptyMR = String::isEmpty;
+        System.out.println("Started with ? 'Mr.' = " + isStartedWithMR.test("Mr."));
+        System.out.println("Started with ? 'Ms.' = " + isStartedWithMR.test("Ms."));
+    }
 
-    System.out.println("Text empty? = " + isEmptyMR.test(""));
-    System.out.println("Text empty? = " + isEmptyMR.test("xyz"));
+    public static void unboundMR() {
+        String pre1 = "Mr.";
+        String pre2 = "Ms.";
+        String name = "Mr. Joe Bloggs";
 
-    BiPredicate<String, String> startsWithLambda = (s1, s2) -> s1.startsWith(s2);
+        Predicate<String> isEmptyLambda = s -> s.isEmpty();
 
-    System.out.println("Is started? (L) = " + startsWithLambda.test(name, pre1));
-    System.out.println("Is started? (L) = " + startsWithLambda.test(name, pre2));
+        System.out.println("Text empty? = " + isEmptyLambda.test(""));
+        System.out.println("Text empty? = " + isEmptyLambda.test("xyz"));
 
-    BiPredicate<String, String> startsWithMR = String::startsWith;
+        Predicate<String> isEmptyMR = String::isEmpty;
 
-    System.out.println("Is started? (MR) = " + startsWithMR.test(name, pre1));
-    System.out.println("Is started? (MR) = " + startsWithMR.test(name, pre2));
-  }
+        System.out.println("Text empty? = " + isEmptyMR.test(""));
+        System.out.println("Text empty? = " + isEmptyMR.test("xyz"));
 
-  public static void constructorMR() {
-    String lambda = "Lambda";
-    String mRef = "Method Reference";
+        BiPredicate<String, String> startsWithLambda = (s1, s2) -> s1.startsWith(s2);
 
-    Supplier<List<String>> suppListLambda = () -> new ArrayList<>();
+        System.out.println("Is started? (L) = " + startsWithLambda.test(name, pre1));
+        System.out.println("Is started? (L) = " + startsWithLambda.test(name, pre2));
 
-    List<String> newList = suppListLambda.get();
-    newList.add(lambda);
+        BiPredicate<String, String> startsWithMR = String::startsWith;
 
-    System.out.println("With L: " + newList);
+        System.out.println("Is started? (MR) = " + startsWithMR.test(name, pre1));
+        System.out.println("Is started? (MR) = " + startsWithMR.test(name, pre2));
+    }
 
-    // method reference
-    Supplier<List<String>> suppListMR = ArrayList::new;
-    newList = suppListMR.get();
-    newList.add(mRef);
+    public static void constructorMR() {
+        String lambda = "Lambda";
+        String mRef = "Method Reference";
 
-    System.out.println("With MR: " + newList);
+        Supplier<List<String>> suppListLambda = () -> new ArrayList<>();
 
-    Function<Integer, List<String>> getListWithLambda = n -> new ArrayList<>(n);
-    newList = getListWithLambda.apply(10);
-    newList.add(lambda);
+        List<String> newList = suppListLambda.get();
+        newList.add(lambda);
 
-    System.out.println("With L + F:" + newList);
+        System.out.println("With L: " + newList);
 
-    // method reference
-    Function<Integer, List<String>> getListWithMR = ArrayList::new;
-    List<String> anotherList = getListWithMR.apply(20);
-    anotherList.add(mRef);
+        // method reference
+        Supplier<List<String>> suppListMR = ArrayList::new;
+        newList = suppListMR.get();
+        newList.add(mRef);
 
-    System.out.println("With MR + F:" + anotherList);
-  }
+        System.out.println("With MR: " + newList);
+
+        Function<Integer, List<String>> getListWithLambda = n -> new ArrayList<>(n);
+        newList = getListWithLambda.apply(10);
+        newList.add(lambda);
+
+        System.out.println("With L + F:" + newList);
+
+        // method reference
+        Function<Integer, List<String>> getListWithMR = ArrayList::new;
+        List<String> anotherList = getListWithMR.apply(20);
+        anotherList.add(mRef);
+
+        System.out.println("With MR + F:" + anotherList);
+    }
 }
--- src/test/java/org/manipulations/test/files/TestFiles.java
+++ src/test/java/org/manipulations/test/files/TestFiles.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.Consumer;
