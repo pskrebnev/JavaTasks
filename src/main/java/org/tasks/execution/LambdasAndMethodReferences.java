07b779ba0708c31a30a5e65e133a661d7b2386a5
PSkrabneu <skrebnev@gmail.com>
2024-06-10 18:09:18 +0200

    Task #5 completed

--- src/main/java/org/tasks/execution/LambdasAndMethodReferences.java
+++ src/main/java/org/tasks/execution/LambdasAndMethodReferences.java
@@ -16,10 +16,12 @@ import java.util.function.Supplier;
 public class LambdasAndMethodReferences {
 
   public static void main(String[] args) {
+    List<Person> listPeople = getPeople();
+
 //    consumer();
 //    supplier();
 //    predicate();
-    function();
+//    function();
 //    staticMR();
 //    boundMR();
 //    unboundMR();
@@ -172,5 +174,14 @@ public class LambdasAndMethodReferences {
     anotherList.add(mRef);
 
     System.out.println("With MR + F:" + anotherList);
+  }
+
+  private static List<Person> getPeople() {
+    List<Person> result = new ArrayList<>();
+    result.add(new Person("Mike", 33, 1.8));
+    result.add(new Person("Mary", 25, 1.4));
+    result.add(new Person("Alan", 34, 1.7));
+    result.add(new Person("Zoe", 30, 1.5));
+    return result;
   }
 }
