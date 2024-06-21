fd9204af6400f32ed4436e3c4c307710277a00ae
PSkrabneu <skrebnev@gmail.com>
2024-06-21 15:15:21 +0200

    Some incompatible features are commented (Before Java )

--- src/main/java/org/tasks/java21/features/PatternMatching.java
+++ src/main/java/org/tasks/java21/features/PatternMatching.java
@@ -1,23 +1,30 @@
 package org.tasks.java21.features;
 
 public class PatternMatching {
-    public static void whatType(Object o){
-        switch(o){ // switch statement does not cover all possible input values
-            case String s -> System.out.println("String");
-            case Integer i -> System.out.println("Integer");
-            case null -> System.out.println("Null");
-            default -> System.out.println("Not recognised");
-        }
-        System.out.println(
-            switch (o) { // switch expression does not cover all possible input values
-                case String s -> "String";
-                case Integer i -> {yield "Integer";}
-                case null -> "Null";
-                default -> "Not recognised";
-            }
-        );
 
+/*
+  public static void whatType(Object o) {
+    switch (o) { // switch statement does not cover all possible input values
+      case String s -> System.out.println("String");
+      case Integer i -> System.out.println("Integer");
+      case null -> System.out.println("Null");
+      default -> System.out.println("Not recognised");
     }
+    System.out.println(
+        switch (o) { // switch expression does not cover all possible input values
+          case String s -> "String";
+          case Integer i -> {
+            yield "Integer";
+          }
+          case null -> "Null";
+          default -> "Not recognised";
+        }
+    );
+
+  }
+*/
+
+/*
     public static void infoOnType(Object o){
         switch(o){
             // Guarded patterns (Java 17, preview) are now replaced by the 'when' clause (Java 19, preview).
@@ -30,25 +37,31 @@ public class PatternMatching {
             default -> System.out.println("Not recognised");
         }
     }
+*/
 
-    public static void exceptionTest(){
-        try{
+/*
+  public static void exceptionTest() {
+    try {
 
-        }catch(Exception e){
-            e.printStackTrace();
+    } catch (Exception e) {
+      e.printStackTrace();
 //        }catch (NullPointerException npe){ // already caught
 //            npe.printStackTrace();
-        }
     }
-    public static void main(String[] args) {
+  }
+*/
+
+/*
+  public static void main(String[] args) {
 //        whatType("ABC");        // String, String
 //        whatType(122);          // Integer, Integer
 //        whatType(null);         // Null, Null
 //        whatType(32.39);        // Not recognised, Not recognised
 
-        infoOnType("ABC");      // String beginning with A : ABC
-        infoOnType("abc");      // Not recognised
-        infoOnType(12);         // Integer > 10 : 12
-        infoOnType(8);          // Not recognised
-    }
+    infoOnType("ABC");      // String beginning with A : ABC
+    infoOnType("abc");      // Not recognised
+    infoOnType(12);         // Integer > 10 : 12
+    infoOnType(8);          // Not recognised
+  }
+*/
 }
