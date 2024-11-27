4646a636e8d2d01c16aa6820c232b7849d6d6a5c
pskrebnev <Kumbaya33>
2024-11-27 10:38:19 +0100

    Palindrome task (not an optimal implementation)

--- src/main/java/org/tasks/stream/TestTask.java
+++ src/main/java/org/tasks/stream/TestTask.java
@@ -1,5 +1,6 @@
 package org.tasks.stream;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -9,6 +10,7 @@ public class TestTask {
 
   // "bab", "aba"
   public static void main(String[] args) {
+//    List<String> palindromes = findPalindromesNO(s);
     List<String> palindromes = findAllPalindromes(s);
     palindromes.forEach(System.out::println);
   }
@@ -18,16 +20,28 @@ public class TestTask {
         .mapToObj(i ->
             IntStream.rangeClosed(0, s.length() - (i + 1))
                 .mapToObj(j -> s.substring(j, j + i + 1))
-                .filter(TestTask::isPalindrome)
                 .filter(str -> str.length() > 2)
+                .filter(TestTask::isPalindrome)
                 .distinct()
-                .toList()
-        )
+                .toList())
         .flatMap(List::stream)
         .toList();
   }
 
   private static boolean isPalindrome(String str) {
     return str.contentEquals(new StringBuilder(str).reverse());
+  }
+
+  public static List<String> findPalindromesNotOptimized(String str) {
+    List<String> listPalindromes = new ArrayList<>();
+    for (int i = 0; i < str.length(); i++) {
+      for (int j = i; j < str.length(); j++) {
+        String s = str.substring(i, j);
+        if (s.length() > 2 && isPalindrome(s)) {
+          listPalindromes.add(s);
+        }
+      }
+    }
+    return listPalindromes;
   }
 }
